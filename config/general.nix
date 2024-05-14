8fd5493a4acbb5ecfcddc21c1be4a7b468b14404
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-05-14 15:13:44 +0530

    Add general vim options

--- config/general.nix
+++ config/general.nix
@@ -1,3 +1,33 @@
+let
+	tabsize = 4;
+in
 {
-  clipboard.register = "unnamedplus";
+	opts = {
+		number = true;
+		relativenumber = true;
+		encoding = "utf-8";
+		hidden = true;
+		ignorecase = true;
+		smartcase = true;
+		incsearch = true;
+		updatetime = 300;
+		history = 1000;
+		mouse = "a";
+		scrolloff = 5;
+		shell = "fish";
+
+		colorcolumn = "100";
+
+		clipboard = "unnamedplus";
+
+		laststatus = 3;
+
+		tabstop = tabsize;
+		softtabstop = tabsize;
+		shiftwidth = tabsize;
+
+		undofile = true;
+	};
+	clipboard.register = "unnamedplus";
+
 }
