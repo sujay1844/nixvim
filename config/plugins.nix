65539256a9742ed9ad16199b8375b507ceccfd03
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-05-23 21:25:36 +0530

    Remove comment cuz new version has it in built and remove toggleterm cuz I'm not using it

--- config/plugins.nix
+++ config/plugins.nix
@@ -3,18 +3,8 @@
 	plugins = {
 		lualine.enable = true;
 		nvim-autopairs.enable = true;
-		comment.enable = true;
 		gitsigns.enable = true;
 		indent-blankline.enable = true;
-		toggleterm = {
-			enable = true;
-			settings = {
-				direction = "float";
-				size = 20;
-				auto_scroll = true;
-				float_opts.border = "curved";
-			};
-		};
 		telescope.enable = true;
 
 		treesitter = {
