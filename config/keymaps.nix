d1573f78009e227cf1394fdad4e96087052f9e1e
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-07-12 19:59:36 +0530

    Remove unused window movement keybindings

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -12,26 +12,6 @@
       action = "<Cmd>ToggleTerm<CR>";
     }
     {
-      mode = [ "n" "t" "i" "v" ];
-      key = "<A-h>";
-      action = "<C-w>h";
-    }
-    {
-      mode = [ "n" "t" "i" "v" ];
-      key = "<A-j>";
-      action = "<C-w>j";
-    }
-    {
-      mode = [ "n" "t" "i" "v" ];
-      key = "<A-k>";
-      action = "<C-w>k";
-    }
-    {
-      mode = [ "n" "t" "i" "v" ];
-      key = "<A-l>";
-      action = "<C-w>l";
-    }
-    {
       mode = "n";
       key = "<C-k>";
       action = ":cprev<CR>";
